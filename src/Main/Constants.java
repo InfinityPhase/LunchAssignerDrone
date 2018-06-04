f995eddc93183ff61a7bd3dc1c779b893eb8af0d
InfinityPhase <4a.zanella@gmail.com>
2018-06-04 10:42:46 -0700

    Rearranged variables for ease of reading

--- src/Main/Constants.java
+++ src/Main/Constants.java
@@ -6,12 +6,12 @@ import java.time.Month;
 
 public final class Constants {
 	// General
-	public static final String PEOPLE_CSV = "LunchPeople.csv";
-	public static final String DEFAULT_CSV_NAME = "NAMEHERE";
-	public static final String ASSIGNMENT_CSV = "Assignments.csv";
-	public static final String PREV_ASSIGNMENT_CSV = "Old_Assignments.csv";
+	
 	public static final int ASSIGNMENT_PEOPLE = 3;
 	public static final int BACKUP_PEOPLE = 2;
+	public static final String ASSIGNMENT_PREFIX = "Assignments";
+	public static final String DATA_STORE_DIR = "credentials/wellshitthissucks.json";
+	public static final String[] DEFAULT_ASSIGNMENT_HEADER = { "Date", "Day Of Week", "Person A", "Person B", "Person C", "Backup A", "Backup B", "Status" };
 	
 	// Value constants
 	public static final double DEFAULT_VALUE = 1.0;
@@ -26,9 +26,14 @@ public final class Constants {
 	public static final DayOfWeek[] DAY_NO_ROTATIONS = { DayOfWeek.SATURDAY, DayOfWeek.SUNDAY, DayOfWeek.MONDAY };
 	public static final LocalDate[] DATE_NO_ROTATIONS = {}; // Not implemented
 	public static final LocalDate[][] RANGE_NO_ROTATIONS = {}; // Not implemented
-	
-	public static final String[] DEFAULT_ASSIGNMENT_HEADER = { "Date", "Day Of Week", "Person A", "Person B", "Person C", "Backup A", "Backup B", "Status" };
-	public static final String DEFAULT_KEY = "";
+		
+	// CSV Settings
+	public static final String DEFAULT_KEY = "1dBnoFECF_iG5YZ9TXmxGaVQlNrnbpR_OyWDT8uFKyh0";
+	public static final String PEOPLE_CSV = "Data";
+	public static final String PEOPLE_CSV_LOCAL = "People.csv";
+	public static final String DEFAULT_CSV_NAME = "NAMEHERE";
+	public static final String ASSIGNMENT_CSV = "Assignments.csv";
+	public static final String PREV_ASSIGNMENT_CSV = "Old_Assignments.csv";
 	public static final String DEFALT_SHEET_NAME = "";
-
+	
 }
